b5e1ddc2957f4a8b0111e4ead9f0c71ae9513c6a
Hritikpatel1 <83468913+Hritikpatel@users.noreply.github.com>
2024-06-02 09:09:06 +0530

    Fixed Allowed Host

--- chatwithpdf/settings.py
+++ chatwithpdf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--v&uu&9ii8565o0v-)xzk^$)13+^b$l+g%sy4nu5+&&mu%o@!u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '*.vercel.app']
 
 
 # Application definition
